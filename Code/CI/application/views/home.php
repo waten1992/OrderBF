62dc9a89dd5bb6f6ea3dda7352b63c794404ee1e
waten <waten1992@gmail.com>
2015-04-21 20:21:13 +0800

    增加meta字段

--- Code/CI/application/views/home.php
+++ Code/CI/application/views/home.php
@@ -2,26 +2,25 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="utf-8" lang="utf-8">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <meta name="Keywords" content="orderbf,定早餐,毕业设计,独立博客" />
         <title>Home</title>
     </head>
     <style>
-
         body {
             width: 35em;
             margin: 0 auto;
             padding: 5px;
             font-family: Tahoma, Verdana, Arial, sans-serif;
         }
-       
 
     </style>
     <body>
         <ul>
-            <li><a href = "http://localhost/index.php/form/home">主页</a></li>
-            <li><a href = "http://localhost/index.php/form/contact">联系方式</a></li>
-            <li><a href = "http://localhost/index.php/form/about">关于Orderbf</a></li>
-            <li><a href = "http://localhost/index.php/form/register">注册</a></li>
-            <li><a href = "http://localhost/index.php/form/login">登录</a></li>
+            <li><a href = "http://localhost/index.php/start/index">主页</a></li>
+            <li><a href = "http://localhost/index.php/start/contact">联系方式</a></li>
+            <li><a href = "http://localhost/index.php/start/about">关于Orderbf</a></li>
+            <li><a href = "http://localhost/index.php/start/register">注册</a></li>
+            <li><a href = "http://localhost/index.php/start/login">登录</a></li>
         </ul>
         <h2>Welcome to Orderbf!</h2>
 
