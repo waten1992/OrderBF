fe5c7230ce2be992dbe7e7d6519ed8bb0a7a2fc4
waten <waten1992@gmail.com>
2015-04-21 19:15:10 +0800

    开启主页的设计模式

---
+++ Code/CI/application/controllers/start.php
@@ -0,0 +1,37 @@
+<?php
+
+class Start extends CI_Controller {
+
+    function index() {
+        $this->load->view('home');
+    }
+
+    function register() {
+        if ($this->form_validation->run('signup') == FALSE) { //通不过验证就提示重新注册
+            $this->load->view('myform');
+        } else { //插入到数据库中
+            $post_data = array(//获取数据
+                'user_id' => $this->input->post('user_id'),
+                'passwd' => $this->input->post('passwd'),
+                'address' => $this->input->post('address')
+            );
+            $post_data['passwd'] = do_hash($this->input->post('passwd'), 'md5'); //默认是SHA1,我选择了MD5 
+            // 数据库中的注册和登录验证的要以相同的方式获取，否者hash出来的东西不一样，比如用：$this->input->post('passwd')
+            $this->db->insert('users', $post_data);
+
+
+            $this->load->view('home'); //默认返回主页
+        }
+    }
+
+    public function user_id_check($tel) { //检查是否是真正的手机号码
+        $iphone = preg_match("/^13[0-9]{1}[0-9]{8}$|15[0189]{1}[0-9]{8}$|189[0-9]{8}$/", $tel);
+        if (!$iphone) {
+            $this->form_validation->set_message('user_id_check', '你的 %s  不对');
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+
+}
---
+++ Code/CI/application/views/home.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="utf-8" lang="utf-8">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Home</title>
+    </head>
+    <style>
+
+        body {
+            width: 35em;
+            margin: 0 auto;
+            padding: 5px;
+            font-family: Tahoma, Verdana, Arial, sans-serif;
+        }
+       
+
+    </style>
+    <body>
+        <ul>
+            <li><a href = "http://localhost/index.php/form/home">主页</a></li>
+            <li><a href = "http://localhost/index.php/form/contact">联系方式</a></li>
+            <li><a href = "http://localhost/index.php/form/about">关于Orderbf</a></li>
+            <li><a href = "http://localhost/index.php/form/register">注册</a></li>
+            <li><a href = "http://localhost/index.php/form/login">登录</a></li>
+        </ul>
+        <h2>Welcome to Orderbf!</h2>
+
+    </body>
+</html>
