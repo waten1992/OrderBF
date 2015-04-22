2b99c6618e3c0c3cd7a1a069e78dd466690a3898
waten <waten1992@gmail.com>
2015-04-23 00:36:01 +0800

    登录和注册页面更改中文的标题

--- Code/CI/application/views/log/login.php
+++ Code/CI/application/views/log/login.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="utf-8" lang="utf-8">
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <title>注册</title>
+        <title>登录</title>
     </head>
     <style>
         body {
@@ -10,6 +10,7 @@
             margin: 0 auto;
             padding: 5px;
             font-family: Tahoma, Verdana, Arial, sans-serif;
+           
         }
 
     </style>
@@ -23,13 +24,13 @@
             <li><a href = "http://localhost/index.php/start/register">注册</a></li>
             <li><a href = "http://localhost/index.php/start/login">登录</a></li>
         </ul>
-        <h2>Welcome to Orderbf!</h2>
+        <h2 class="pink">Welcome to Orderbf!</h2>
       
       
       
         <div>
             <td style="padding-left: 50px">
-                <form name="input" action="http://localhost/index.php/start/register" method="post">
+                <form name="input" action="http://localhost/index.php/start/login" method="post">
                 <table>
                     <tr>
                         <?php echo form_error('user_id'); ?>
@@ -42,32 +43,13 @@
                         <td>密码：</td>
                         <td><input type="password" name="passwd"  value="<?php echo set_value('passwd'); ?>" /></td>
                     </tr>
-                    
-                    <tr>
-                         <?php echo form_error('passconf');  ?>
-                        <td>密码确认：</td>
-                        <td><input type="password" name="passconf" value="<?php echo set_value('passconf'); ?>" /></td>
-                    </tr>
-                    
-                    <tr>
-                        <?php echo form_error('email'); ?> 
-                        <td>邮箱：</td>
-                        <td><input type="email" name="email" value="<?php echo set_value('email'); ?>" /></td>
-                    </tr>
-                    
-                     <tr>
-                        <?php echo form_error('address'); ?> 
-                        <td>地址：</td>
-                        <td><input type="text" name="address" value="<?php echo set_value('address'); ?>" /></td>
-                    </tr>
-                    
                     </table>
                     <div><input type="submit" value='submit' /></div>
                    </form>
                 </td>
                                     
         </div>
-        
+        <a href = "http://localhost/index.php/start/forget">忘记密码</a>
 
                         </body>
                         </html>
