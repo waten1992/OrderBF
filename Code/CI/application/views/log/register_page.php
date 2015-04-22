e28ff593fe785009b34f9293fa396d753d503a4d
waten <waten1992@gmail.com>
2015-04-22 08:43:08 +0800

    新建注册页面

---
+++ Code/CI/application/views/log/register_page.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="utf-8" lang="utf-8">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <title>Home</title>
+    </head>
+    <style>
+        body {
+            width: 35em;
+            margin: 0 auto;
+            padding: 5px;
+            font-family: Tahoma, Verdana, Arial, sans-serif;
+        }
+
+    </style>
+    <body>
+        <ul>
+            <li><a href = "http://localhost/index.php/start/index">主页</a></li>
+            <li><a href = "http://localhost/index.php/start/contact">联系方式</a></li>
+            <li><a href = "http://localhost/index.php/start/about">关于Orderbf</a></li>
+            <li><a href = "http://localhost/index.php/start/register">注册</a></li>
+            <li><a href = "http://localhost/index.php/start/login">登录</a></li>
+        </ul>
+        <h2>Welcome to Orderbf!</h2>
+        <div>
+            <td style="padding-left: 50px">
+            <table>
+                <tr>
+                    <td>管理员姓名：</td>
+                    <td><input type="text" name="username" /></td>
+                </tr>
+                <tr>
+                    <td>管理员密码：</td>
+                    <td><input type="password" name="password" /></td>
+                </tr>
+                <tr>
+                    <td>验证码：</td>
+                    <td><input type="text" name="captcha" class="capital" /></td>
+                </tr>
+            <tr>
+            
+            </div>
+
+
+    </body>
+</html>
+
