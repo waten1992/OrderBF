911aac113d2a074dc05f338200fc576eea9b20da
waten <waten1992@gmail.com>
2015-05-08 10:40:35 +0800

    主页增加商品

--- Code/CI/application/views/home.php
+++ Code/CI/application/views/home.php
@@ -57,7 +57,7 @@
             </div>
         </div>
 
-        <div class="row">
+               <div class="row">
             <div class="col-sm-6 col-md-4">
                 <div class="thumbnail">
                     <img src="http://localhost/images/pd/Soybean.png" alt="这个是豆浆" style="width: 350px; height: 350px;">
@@ -66,6 +66,7 @@
                         <p style="color: red">价格:<?php echo $soybean; ?>￥</p>
                         <p style="color: green;">还剩：<?php echo $soybean_capacity; ?></p>
                         <p><?php echo $soybean_pd; ?></p>
+                        <p style="color: blue;"><?php echo $soybean_content; ?> </p>
                         <form action="http://localhost/index.php/shop_cart/add">
                             数量: <input type="number" name="qty" min="1" max=<?php echo $soybean_capacity; ?> value="1"/>
                             <input type="hidden" name="capacity" value=<?php echo $soybean_capacity; ?> />
@@ -75,33 +76,58 @@
                             <input type="submit"  method="get" class="btn btn-primary" role="button" value='加入购物车' />
                         </form>
                         <br>
-                        <p> <a href="#" class="btn btn-info" role="button">选我,我最好喝了啦</a></p>
+                        <p><a  href="http://localhost/index.php/comments/index/<?php echo $soybean_id; ?>" class="btn btn-info" role="button">看看评论吧～</a> </p>
                     </div>
                 </div>
             </div>
             <div class="col-sm-6 col-md-4">
                 <div class="thumbnail">
-                    <img src="http://localhost/images/pd/bread01.jpg" alt="这个是面包" style="width: 350px; height: 350px;">
+                    <img src="http://localhost/images/pd/bread01.jpg"  alt="这个是面包" style="width: 350px; height: 350px;">
                     <div class="caption">
                         <h3 style="color: green"><?php echo $bread_name; ?></h3>
                         <p style="color: red">价格:<?php echo $bread; ?>￥</p>
-                           <p style="color: green;">还剩：<?php echo $bread_capacity; ?></p>
+                        <p style="color: green;">还剩：<?php echo $bread_capacity; ?></p>
                         <p><?php echo $bread_pd; ?></p>
-                        <form action="http://localhost/index.php/shop_cart/add/200">
+                        <p style="color: blue;"><?php echo $bread_content; ?> </p>
+                        <form action="http://localhost/index.php/shop_cart/add">
                             数量: <input type="number" name="qty" min="1" max=<?php echo $bread_capacity; ?> value="1"/>
-                              <input type="hidden" name="capacity" value=<?php echo $bread_capacity; ?> />
+                            <input type="hidden" name="capacity" value=<?php echo $bread_capacity; ?> />
                             <input type="hidden" name="item_id" value="200" />
                             <input type="hidden" name="item_name" value= <?php echo $bread_name; ?> />
                             <input type="hidden" name="price" value= <?php echo $bread; ?> />
                             <input type="submit"  method="get" class="btn btn-primary" role="button" value='加入购物车' />
                         </form>
                         <br/>
-                        <p><a href="#" class="btn btn-info" role="button">选我啦，我最好吃啦</a> </p>
+                        <p><a  href="http://localhost/index.php/comments/index/<?php echo $bread_id; ?>" class="btn btn-info" role="button">看看评论吧～</a> </p>
                     </div>
                 </div>
             </div>
-            
-             <div class="col-sm-6 col-md-4">
+
+
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="http://localhost/images/pd/corn.gif" alt="这个是玉米" style="width: 350px; height: 350px;">
+                    <div class="caption">
+                        <h3 style="color: green"><?php echo $corn_name; ?></h3>
+                        <p style="color: red">价格:<?php echo $corn_price; ?>￥</p>
+                        <p style="color: green;">还剩：<?php echo $corn_capacity; ?></p>
+                        <p><?php echo $corn_pd; ?></p>
+                        <p style="color: blue;"><?php echo $corn_content; ?> </p>
+                        <form action="http://localhost/index.php/shop_cart/add/400">
+                            数量: <input type="number" name="qty" min="1" max=<?php echo $corn_capacity; ?> value="1"/>
+                            <input type="hidden" name="capacity" value=<?php echo $corn_capacity; ?> />
+                            <input type="hidden" name="item_id" value="400" />
+                            <input type="hidden" name="item_name" value= <?php echo $corn_name; ?> />
+                            <input type="hidden" name="price" value= <?php echo $corn_price; ?> />
+                            <input type="submit"  method="get" class="btn btn-primary" role="button" value='加入购物车' />
+                        </form>
+                        <br/>
+                        <p><a  href="http://localhost/index.php/comments/index/<?php echo $corn_id; ?>" class="btn btn-info" role="button">看看评论吧～</a> </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-md-4">
                 <div class="thumbnail">
                     <img src="http://localhost/images/pd/Tomato.gif" alt="这个是番茄汁" style="width: 350px; height: 350px;">
                     <div class="caption">
@@ -109,21 +135,71 @@
                         <p style="color: red">价格:<?php echo $tomato_price; ?>￥</p>
                         <p style="color: green;">还剩：<?php echo $tomato_capacity; ?></p>
                         <p><?php echo $tomato_pd; ?></p>
+                        <p style="color: blue;"><?php echo $tomato_content; ?> </p>
                         <form action="http://localhost/index.php/shop_cart/add/300">
                             数量: <input type="number" name="qty" min="1" max=<?php echo $tomato_capacity; ?> value="1"/>
-                              <input type="hidden" name="capacity" value=<?php echo $tomato_capacity; ?> />
+                            <input type="hidden" name="capacity" value=<?php echo $tomato_capacity; ?> />
                             <input type="hidden" name="item_id" value="300" />
                             <input type="hidden" name="item_name" value= <?php echo $tomato_name; ?> />
                             <input type="hidden" name="price" value= <?php echo $tomato_price; ?> />
                             <input type="submit"  method="get" class="btn btn-primary" role="button" value='加入购物车' />
                         </form>
                         <br/>
-                        <p><a href="#" class="btn btn-info" role="button">选我啦，我最好吃啦</a> </p>
+                        <p><a  href="http://localhost/index.php/comments/index/<?php echo $tomato_id; ?>" class="btn btn-info" role="button">看看评论吧～</a> </p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="http://localhost/images/pd/carrot.gif" alt="这个是胡萝卜" style="width: 350px; height: 350px;">
+                    <div class="caption">
+                        <h3 style="color: green"><?php echo $carrot_name; ?></h3>
+                        <p style="color: red">价格:<?php echo $carrot_price; ?>￥</p>
+                        <p style="color: green;">还剩：<?php echo $carrot_capacity; ?></p>
+                        <p><?php echo $carrot_pd; ?></p>
+                        <p style="color: blue;"><?php echo $carrot_content; ?> </p>
+                        <form action="http://localhost/index.php/shop_cart/add/301">
+                            数量: <input type="number" name="qty" min="1" max=<?php echo $carrot_capacity; ?> value="1"/>
+                            <input type="hidden" name="capacity" value=<?php echo $carrot_capacity; ?> />
+                            <input type="hidden" name="item_id" value="301" />
+                            <input type="hidden" name="item_name" value= <?php echo $carrot_name; ?> />
+                            <input type="hidden" name="price" value= <?php echo $carrot_price; ?> />
+                            <input type="submit"  method="get" class="btn btn-primary" role="button" value='加入购物车' />
+                        </form>
+                        <br/>
+                        <p><a  href="http://localhost/index.php/comments/index/<?php echo $carrot_id; ?>" class="btn btn-info" role="button">看看评论吧～</a> </p>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="col-sm-6 col-md-4">
+                <div class="thumbnail">
+                    <img src="http://localhost/images/pd/mix_juice.gif" alt="这个是混合果汁" style="width: 350px; height: 350px;">
+                    <div class="caption">
+                        <h3 style="color: green"><?php echo $mix_juice_name; ?></h3>
+                        <p style="color: red">价格:<?php echo $mix_juice_price; ?>￥</p>
+                        <p style="color: green;">还剩：<?php echo $mix_juice_capacity; ?></p>
+                        <p><?php echo $mix_juice_pd; ?></p>
+                        <p style="color: blue;"><?php echo $mix_juice_content; ?> </p>
+                        <form action="http://localhost/index.php/shop_cart/add/500">
+                            数量: <input type="number" name="qty" min="1" max=<?php echo $mix_juice_capacity; ?> value="1"/>
+                            <input type="hidden" name="capacity" value=<?php echo $mix_juice_capacity; ?> />
+                            <input type="hidden" name="item_id" value="500" />
+                            <input type="hidden" name="item_name" value= <?php echo $mix_juice_name; ?> />
+                            <input type="hidden" name="price" value= <?php echo $mix_juice_price; ?> />
+                            <input type="submit"  method="get" class="btn btn-primary" role="button" value='加入购物车' />
+                        </form>
+                        <br/>
+                        <p><a  href="http://localhost/index.php/comments/index/<?php echo $mix_juice_id; ?>" class="btn btn-info" role="button">看看评论吧～</a> </p>
                     </div>
                 </div>
             </div>
 
         </div>
+
 
 
 
